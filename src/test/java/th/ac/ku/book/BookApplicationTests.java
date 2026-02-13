fb12266219b6c21e5e09520205d970247e9a50bd
Usa Sammapun <usa.sa@ku.th>
2026-02-13 11:40:39 +0700

    add test

--- src/test/java/th/ac/ku/book/BookApplicationTests.java
+++ src/test/java/th/ac/ku/book/BookApplicationTests.java
@@ -3,11 +3,15 @@ package th.ac.ku.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BookApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// จำลองการทดสอบ
+		assertTrue(true);
 	}
 
 }
